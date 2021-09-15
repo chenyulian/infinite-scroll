aa0fcd8b8d573ac47fb6cf65ea941e7073c69b1c
kinppink <648270560@qq.com>
2021-09-15 19:27:46 +0800

    use async/await & complete infinite scroll

--- main.js
+++ main.js
@@ -3,32 +3,47 @@ const loading = document.querySelector(".loader");
 const filter = document.querySelector("#filter");
 const url = "https://jsonplaceholder.typicode.com";
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
-function getPosts() {
-  return fetch(`${url}/posts?_limit=${limit}&_page=${page}`).then(
-    (response) => response.json(),
-    (error) => {
-      alert(error);
-    }
-  );
+async function getPosts() {
+  const res = await fetch(`${url}/posts?_limit=${limit}&_page=${page}`);
+  const data = await res.json();
+  return data;
 }
+
 // show posts in DOM
-function showPosts() {
-  getPosts().then((data) => {
-    console.log(data);
-    data.forEach((post) => {
-      const postEl = document.createElement("div");
-      postEl.classList.add("post");
-      postEl.innerHTML = `<div class="number">${post.id}</div>
-                        <div class="post-info">
-                            <div class="post-title">${post.title}</div>
-                            <p class="post-body">${post.body}</p>
-                        </div>`;
-      postsContainer.appendChild(postEl);
-    });
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `<div class="number">${post.id}</div>
+      <div class="post-info">
+          <div class="post-title">${post.title}</div>
+          <p class="post-body">${post.body}</p>
+      </div>`;
+    postsContainer.appendChild(postEl);
   });
 }
 
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// show initial posts
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
